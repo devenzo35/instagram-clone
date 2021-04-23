c4887334e026c05e9fe3dfa4046387690bb784fa
Enzo <enzocuellar12@gmail.com>
2021-04-22 23:42:28 -0300

    Tailwindcss implemented

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="bg-white border w-full h-14 shadow-sm flex flex-row justify-evenly items-center p-2">
+            <img className="w-32" src="/definitive.png" />
+        </nav>
+    )
+}
---
+++ components/Stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Stories = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
