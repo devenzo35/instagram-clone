2b5f7ffa06dc18dc14889089e73d2c97d02a8a0b
Enzo <enzocuellar12@gmail.com>
2021-05-17 16:18:46 -0300

    several improves on profile page

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -6,9 +6,11 @@ export const ProfileMenu: FC = () => {
     <nav className="bg-white shadow-md duration-500 w-52 h-52 absolute top-12 right-0 rounded-xl ">
       <i className="fas fa-chevron-up absolute right-6"></i>
       <ul className="h-full flex flex-col justify-evenly items-start p-2">
-        <li>
-          <i className="far fa-user-circle"></i> profile
-        </li>
+        <Link href="/profile">
+          <li className="w-full cursor-pointer hover:bg-gray-100">
+            <i className="far fa-user-circle"></i> profile
+          </li>
+        </Link>
         <li>
           <i className="far fa-bookmark"></i> saved
         </li>
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,15 +2,39 @@ import React from "react";
 
 export default function Profile() {
   return (
-    <div className="bg-gray-300 w-4/6 m-auto mt-14 h-screen">
-      <section className="border-b-2 h-2/6 flex flex-row items-center justify-around">
-        <div className="rounded-full w-36 h-36 bg-gray-400"></div>
-        <div>
-          <span>Enzocuellar35</span>
-          <button>Edit profile</button>
-          <i className="fas fa-cog"></i>
-        </div>
+    <div className="w-4/6 m-auto mt-14 h-screen">
+      <section className="border-b-2 h-2/6 flex flex-row items-center justify-start">
+        <div className="rounded-full w-36 h-36 bg-gray-400 ml-24"></div>
+        <section className="ml-24 w-4/12 h-28 flex flex-col justify-between">
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-light">enzocuellar35</span>
+            <button className="border rounded-md p-0.5 px-1.5 font-semibold">
+              Edit profile
+            </button>
+            <i className="fas fa-cog"></i>
+          </div>
+          <div className="flex items-center justify-between">
+            <span>0 posts</span>
+            <span>49 followers</span>
+            <span>117 following</span>
+          </div>
+          <span className="font-semibold">Enzo Fabricio Cuellar</span>
+        </section>
       </section>
+      <nav className="w-5/12 h-12 m-auto flex flex-row justify-between text-gray-400 font-semibold text-xs">
+        <li className="w-1/6 grid place-items-center cursor-pointer border-t hover:border-black">
+          POSTS
+        </li>
+        <li className="w-1/6 grid place-items-center cursor-pointer hover:border-t-2">
+          IGTV
+        </li>
+        <li className="w-1/6 grid place-items-center cursor-pointer hover:border-t-2">
+          SAVED
+        </li>
+        <li className="w-1/6 grid place-items-center cursor-pointer hover:border-t-2">
+          TAGGED
+        </li>
+      </nav>
     </div>
   );
 }
